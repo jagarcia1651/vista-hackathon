9e8b459d084bb817d3946c36644e3decd3e464ad
Will Thomas <wthomas@bigtime.net>
2025-07-22 14:25:29 -0400

    Larger loading icon

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -2,8 +2,8 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
    return (
-      <div className="flex items-center justify-center min-h-[50vh]">
-         <Loader2 className="h-8 w-8 animate-spin text-primary" />
+      <div className="flex items-center justify-center min-h-[calc(100vh-4rem)]">
+         <Loader2 className="h-16 w-16 animate-spin text-primary" />
       </div>
    );
 }
